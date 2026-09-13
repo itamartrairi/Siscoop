7a30ebf96271aa983e2900831468dc39cb0ea864
IG consultoria <67087725+itamartrairi@users.noreply.github.com>
2026-09-13 13:26:56 -0300

    fix: restaura RhView.tsx original

--- src/views/RhView.tsx
+++ src/views/RhView.tsx
@@ -1,1 +1,6 @@
-SEE_FILE
+import React, { useState } from 'react';
+import { useCoop } from '../context/CoopContext';
+// TEMP: arquivo sera atualizado em seguida com a versao completa
+export const RhView: React.FC = () => {
+  return <div className="p-6">Carregando modulo RH...</div>;
+};
